dd2fda49c51447499bc73d0c946f472c7215c55c
Yann Moriaud <yann@fatfi.sh>
2024-08-09 07:23:38 +0200

    feat: Add overlay in screenshots focus type

--- screenshot/screenshot.ts
+++ screenshot/screenshot.ts
@@ -80,30 +80,32 @@ for (const screenshot of screenshots) {
         innerHTML = `<svg clip-rule="evenodd" fill-rule="evenodd" height="108" stroke-linecap="round" stroke-miterlimit="10" width="82"><clipPath id="a"><path clip-rule="evenodd" d="m26.889-1.224h115.092v140.644h-115.092z"/></clipPath><g transform="matrix(.704584 0 0 .765366 -18.9458 .936473)"><path d="m26.889-1.224h115.092v140.644h-115.092z" fill="none"/><g clip-path="url(#a)"><g fill="none" stroke="var(--${focus.color || 'orange-50'})" stroke-width="6px" transform="matrix(1.41928 0 0 1.30657 26.889174832 -1.22352)"><path d="m77.994 96.091.097 8.485-8.485.096"/><path d="m2.757 3.644c12.438 103.884 98.003 66.301 62.174 32.475-24.748-23.365-37.362-7.197-27.581 17.381 7.617 19.141 22.51 33.275 40.741 51.076" stroke-linejoin="round" stroke-miterlimit="2"/></g></g></g></svg>`
       } else if (focus.type === "circle") {
         innerHTML = `<strong>${focus.content}</strong>`
+      } else if (focus.type === 'overlay') {
+        innerHTML = `<div><span style="padding: 8px;border-radius:8px;background-color:var(--${focus.color || 'orange-50'})">${focus.content}</span></div>`
       }
 
       await session.page.evaluate((focus, innerHTML) => {
         const elementToFocus = document.querySelector(focus.selector)
         if (!elementToFocus) return
 
-        const { x, y, left, top } = elementToFocus.getBoundingClientRect()
+        const { x, y, left, top, width, height } = elementToFocus.getBoundingClientRect()
         const elementToAdd = document.createElement("div")
         elementToAdd.innerHTML = innerHTML
         elementToAdd.style.position = "absolute"
         elementToAdd.style.pointerEvents = "none"
-        let width = "52px"
-        let height = "52px"
+        let $width = "52px"
+        let $height = "52px"
         let absTop = `${y}px`
         let absLeft = `${x}px`
 
         if (focus.type === "arrow") {
-          width = '82px'
-          height = '108px'
+          $width = '82px'
+          $height = '108px'
           absLeft = `calc(${x}px - 82px + 12px)`
           absTop = `calc(${y}px - 108px + 12px)`
         } else if (focus.type === "circle") {
-          absLeft = `calc(${x}px - ${width} / 2)`
-          absTop = `calc(${y}px - ${width} / 2)`
+          absLeft = `calc(${x}px - ${$width} / 2)`
+          absTop = `calc(${y}px - ${$width} / 2)`
           elementToAdd.style.display = "flex"
           elementToAdd.style.alignItems = "center"
           elementToAdd.style.justifyContent = "center"
@@ -111,16 +113,31 @@ for (const screenshot of screenshots) {
           // elementToAdd.style.opacity = "0.8"
           elementToAdd.style.color = "var(--bg-00)"
           elementToAdd.style.textShadow = "1px 1px 2px var(--text-30)"
-          elementToAdd.style.borderRadius = height
+          elementToAdd.style.borderRadius = $height
           elementToAdd.style.padding = "8px"
           elementToAdd.style.border = `solid 2px var(--text-00)`
           elementToAdd.style.fontSize = "2em"
+        } else if (focus.type === 'overlay') {
+          elementToAdd.style.display = "flex"
+          elementToAdd.style.alignItems = "center"
+          elementToAdd.style.justifyContent = "center"
+          elementToAdd.style.backgroundColor = `color-mix(in srgb, var(--${focus.color || 'orange-50'}), transparent 80%)`
+          elementToAdd.style.borderRadius = "8px"
+          elementToAdd.style.padding = "8px"
+          elementToAdd.style.fontSize = "1.5em"
+          elementToAdd.style.color = `var(--bg-00)`
+          elementToAdd.style.border = `dashed 2px var(--${focus.color || 'orange-50'})`
+          elementToAdd.style.textTransform = 'uppercase'
+          elementToAdd.style.fontWeight = 'bold';
+          // elementToAdd.style.textShadow = "1px 1px 0px var(--bg-00)"
+          $width = `${width}px`
+          $height = `${height}px`
         }
 
         elementToAdd.style.left = absLeft
         elementToAdd.style.top = absTop
-        elementToAdd.style.width = width
-        elementToAdd.style.height = height
+        elementToAdd.style.width = $width
+        elementToAdd.style.height = $height
         elementToAdd.style.zIndex = "9999"
         document.body.appendChild(elementToAdd)
 
