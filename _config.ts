70d1bd70617a5a5c81c09333e3800a4e68c26687
Yann Moriaud <yann@fatfi.sh>
2024-08-09 07:22:01 +0200

    refacto: Clean _config file

--- _config.ts
+++ _config.ts
@@ -1,26 +1,60 @@
 import lume from "lume/mod.ts";
 
-import nav from "lume/plugins/nav.ts";
-import resolveUrls from "lume/plugins/resolve_urls.ts";
-import toc from "https://deno.land/x/lume_markdown_plugins@v0.7.0/toc.ts";
-
-import attributes from "lume/plugins/attributes.ts";
-import sass from "lume/plugins/sass.ts";
-
-import digitalOceanMd from "npm:@digitalocean/do-markdownit";
-import multilanguage from "lume/plugins/multilanguage.ts";
-import pagefind from "lume/plugins/pagefind.ts";
-import prism from "lume/plugins/prism.ts";
-import 'npm:prismjs@1.29.0/components/prism-json.js';
-import 'npm:prismjs@1.29.0/components/prism-python.js';
+// Custom plugins
 import mermaid from "./src/_plugins/markdown/mermaid.js";
 import card from "./src/_plugins/markdown/card.js";
 import brunoLoader from "./src/_plugins/bruno.ts";
 
+// Plugins
+import robots from "lume/plugins/robots.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import multilanguage from "lume/plugins/multilanguage.ts";
+import resolveUrls from "lume/plugins/resolve_urls.ts";
+import nav from "lume/plugins/nav.ts";
+import sass from "lume/plugins/sass.ts";
+import pagefind from "lume/plugins/pagefind.ts";
 
-const markdown = {
-  plugins: [mermaid, card, [digitalOceanMd, { prismjs: false, user_mention: false }]],
-};
+// Markdown plugins
+import toc from "https://deno.land/x/lume_markdown_plugins@v0.7.0/toc.ts";
+import digitalOceanMd from "npm:@digitalocean/do-markdownit";
+import prism from "lume/plugins/prism.ts";
+import 'npm:prismjs@1.29.0/components/prism-json.js';
+import 'npm:prismjs@1.29.0/components/prism-python.js';
+import 'npm:prismjs@1.29.0/components/prism-bash.js';
+
+const options = {
+  robots: {
+    disallow: "/pagefind",
+  },
+  markdown: {
+    plugins: [mermaid, card, [digitalOceanMd, { prismjs: false, user_mention: false }]],
+  },
+  toc: {
+    level: 1
+  },
+  pagefind: {
+    ui: {
+      containerId: "search",
+      showImages: true,
+      excerptLength: 0,
+      showEmptyFilters: false,
+      showSubResults: false,
+      resetStyles: true,
+      openFilters: ['topic']
+    },
+    indexing: {
+      verbose: false,
+    },
+  },
+  sass: {
+    extensions: [".scss"],
+    includes: "_includes/styles",
+  },
+  multilanguage: {
+    languages: ["en", "fr"],
+    defaultLanguage: "en",
+  }
+}
 
 const site = lume({
   src: "./src",
@@ -31,43 +65,26 @@ const site = lume({
   watcher: {
     ignore: [
       "/.git",
-      (path) => path.endsWith("/.DS_Store"),
-      (path) => path.endsWith("/_screenshots.json"),
+      (path: string) => path.endsWith("/.DS_Store"),
+      (path: string) => path.endsWith("/_screenshots.json"),
     ],
   },
-}, { markdown });
+}, { markdown: options.markdown });
+
 
 site.use(resolveUrls());
-site.use(toc({
-  level: 1
-}));
-site.use(pagefind({
-  ui: {
-    containerId: "search",
-    showImages: true,
-    excerptLength: 0,
-    showEmptyFilters: false,
-    showSubResults: false,
-    resetStyles: true,
-    openFilters: ['topic']
-  }
-}));
+site.use(toc(options.toc));
 site.use(nav());
-site.use(attributes());
-site.use(sass({
-  extensions: [".scss"],
-  includes: "_includes/styles",
-}));
+site.use(sass(options.sass));
 site.use(prism());
 
-site.loadPages([".bru"], {
-  loader: brunoLoader
-})
+site.loadPages([".bru"], brunoLoader)
 
-site.use(multilanguage({
-  languages: ["en", "fr"],
-  defaultLanguage: "en",
-}))
+site.use(multilanguage(options.multilanguage));
+site.use(sitemap());
+site.use(robots(options.robots));
+
+site.use(pagefind(options.pagefind));
 
 site.copy("_medias");
 site.copy("_styles");
