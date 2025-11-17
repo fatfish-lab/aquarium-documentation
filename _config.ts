2a3e2769b3ebe3ed5313e15784b137923c3dfa2d
Yann Moriaud <yann@fatfi.sh>
2025-11-17 18:54:32 +0100

    feat: Add posthog tracking

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts"
 
 import documentation from "documentation/mod.ts"
+import { posthog } from "posthog/mod.ts"
 
 const site = lume({
   src: "./src",
@@ -15,6 +16,15 @@ const options = {
 }
 
 site.use(documentation(options))
+site.use(posthog({
+  apiKey: Deno.env.get("POSTHOG_API_KEY") ?? "",
+  options: {
+    api_host: Deno.env.get("POSTHOG_API_HOST") ?? "",
+    ui_host: Deno.env.get("POSTHOG_UI_HOST") ?? "",
+    defaults: "2025-05-24",
+    person_profiles: "identified_only",
+  }
+}))
 site.copy("_medias")
 
 export default site
